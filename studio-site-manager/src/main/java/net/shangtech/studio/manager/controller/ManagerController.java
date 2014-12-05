30272c24388d6435683e9240052182c81185a4fe
songxh <tsingheng@163.com>
2014-12-05 17:39:01 +0800

    add login page

--- studio-site-manager/src/main/java/net/shangtech/studio/manager/controller/ManagerController.java
+++ studio-site-manager/src/main/java/net/shangtech/studio/manager/controller/ManagerController.java
@@ -3,10 +3,14 @@ package net.shangtech.studio.manager.controller;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+
+import javax.servlet.http.HttpSession;
 
 import net.shangtech.framework.controller.AjaxResponse;
 import net.shangtech.framework.file.FileSaver;
 import net.shangtech.framework.file.GenericFile;
+import net.shangtech.framework.util.EncoderUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -21,6 +27,20 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 public class ManagerController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ManagerController.class);
+	
+	private static final Properties props = new Properties();
+	
+	private static final String USERNAME_KEY = "username";
+	private static final String PASSWORD_KEY = "password";
+	public static final String SESSION_USER_KEY = "user_in_session";
+	
+	static{
+		try {
+			props.load(ManagerController.class.getClassLoader().getResourceAsStream("config/auth.properties"));
+		} catch (IOException e) {
+			logger.error("加载管理员账户文件异常", e);
+		}
+	}
 	
 	@Autowired private FileSaver fileSaver;
 
@@ -47,6 +67,39 @@ public class ManagerController {
 		});
 		ajaxResponse.setSuccess(true);
 		ajaxResponse.setData(map);
+		return ajaxResponse;
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(){
+		return "manager.login";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@RequestParam String username, @RequestParam String password, Model model, HttpSession session){
+		if(username.equals(props.getProperty(USERNAME_KEY))
+				&& EncoderUtils.MD5(username + "@" + password).equals(props.getProperty(PASSWORD_KEY))){
+			Map<String, String> user = new HashMap<>();
+			user.put(USERNAME_KEY, username);
+			session.setAttribute(SESSION_USER_KEY, user);
+			return "manager.index";
+		}
+		else{
+			model.addAttribute("msg", "账号或密码错误");
+			return "manager.login";
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping("/reset")
+	public AjaxResponse reset(@RequestParam String oldUsername, @RequestParam String username, @RequestParam String oldPassword, @RequestParam String password){
+		AjaxResponse ajaxResponse = AjaxResponse.instance();
+		if(oldUsername.equals(props.getProperty(USERNAME_KEY))
+				&& EncoderUtils.MD5(oldUsername + "@" + oldPassword).equals(props.getProperty(PASSWORD_KEY))){
+			props.setProperty(USERNAME_KEY, username);
+			props.setProperty(PASSWORD_KEY, EncoderUtils.MD5(username + "@" + password));
+			ajaxResponse.setSuccess(true);
+		}
 		return ajaxResponse;
 	}
 	
---
+++ studio-site-manager/src/main/webapp/WEB-INF/page/manager-login.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/page/commons/tags.jsp"%>
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>网站后台</title>
+	
+	<!-- Bootstrap -->
+	<link href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.css" rel="stylesheet">
+	<link href="${ctx}/css/studio-manager.css" rel="stylesheet">
+	
+	<script type="text/javascript" src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+      <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script type="text/javascript">var ctx = '${ctx}';</script>
+</head>
+<body>
+<div class="login-wrapper">
+	<div class="modal">
+  		<div class="modal-dialog">
+    		<div class="modal-content">
+      			<div class="modal-body">
+        			<form class="form">
+        				<div class="form-group">
+        					<div class="input-group">
+        						<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+        						<input type="text" name="username" class="form-control" placeholder="Username">
+        					</div>
+        				</div>
+        				<div class="form-group">
+        					<div class="input-group">
+        						<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+        						<input type="text" name="password" class="form-control" placeholder="Password">
+        					</div>
+        				</div>
+        			</form>
+      			</div>
+      			<div class="modal-footer">
+        			<a href="javascript:;" class="btn btn-primary submit"> 登  录  后  台  </a>
+      			</div>
+    		</div>
+ 		</div>
+	</div>
+</div>
+</body>
